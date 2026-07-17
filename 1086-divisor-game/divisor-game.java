4529fa862649203e7c6010728a6e24371ab8d3ed
Amit Dubey <amitdubey04102004@gmail.com>
2026-07-17 22:14:19 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (96.86%) - LeetSync

--- 1086-divisor-game/divisor-game.java
+++ 1086-divisor-game/divisor-game.java
@@ -1,5 +1,9 @@
 class Solution {
     public boolean divisorGame(int n) {
-      return n % 2 ==0;  
-    }
+       return n % 2 ==0 ;
+    //   return n - x;
+       }
+        
+//   return false;
+    
 }
